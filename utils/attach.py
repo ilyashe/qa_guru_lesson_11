be6e045c62720db1a58daa6971f63f1b300b7008
ilyashe <ilya.shebanoff@gmail.com>
2025-03-06 17:51:44 +0200

    change add_video

--- utils/attach.py
+++ utils/attach.py
@@ -18,7 +18,7 @@ def add_html(browser):
 
 # скринкаст
 def add_video(browser, selenoid_url):
-    video_url = f"{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
+    video_url = f"https://{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
