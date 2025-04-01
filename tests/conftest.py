4c4c42225be69d0b23664f557fe509a99ec6e3a1
ilyashe <ilya.shebanoff@gmail.com>
2025-04-01 12:46:08 +0300

    change default browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def setting_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "128.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
