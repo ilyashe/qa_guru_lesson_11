96e6261d20c7ed59e8a831ba639941ad934736ec
ilyashe <ilya.shebanoff@gmail.com>
2025-04-01 12:43:16 +0300

    change default browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ import os
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version', help='Версия браузера, в которой будут запущены тесты',
-        default='100.0'
+        default='128.0'
     )
 
 
